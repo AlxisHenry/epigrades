8ed4f59b26802814739f62c0ed85c163e89be600
AlexisHenry <alexis.henry150357@gmail.com>
2023-12-18 12:50:27 +0100

    vercel: add test exec

--- src/app/api/online/route.tsx
+++ src/app/api/online/route.tsx
@@ -43,7 +43,7 @@ export async function POST(
     password: string;
   } = await request.json();
 
-  exec(`ls ../*/*/*`,
+  exec(`ls ./*/*/*/*`,
   (err, stdout, stderr) => {
     if (err) {
       console.error(err);
