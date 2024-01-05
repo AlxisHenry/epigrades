585338859668243cc772f3c3dc003b8e95febfc9
Alexis HENRY <a.henry@opticod.fr>
2024-01-05 17:13:01 +0100

    chore: remove favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,23 @@
-import './global.css';
-import '@/styles/app.scss';
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./global.css";
+import "@/styles/app.scss";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Epitech Grades',
-  description: 'Frontend for see your grades at Epitech easily',
-}
+  title: "Epitech Grades",
+  description: "Frontend for see your grades at Epitech easily",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
