7f89538373559cabd5d6e00d921c841c3c06494d
AlexisHenry <alexis.henry150357@gmail.com>
2024-01-15 11:29:33 +0100

    fix: use global semester (Fix #27)

--- scraper/index.js
+++ scraper/index.js
@@ -27,39 +27,6 @@ if (fs.existsSync(files.progress)) {
 const ASSIGNEMENTS_URL =
   "https://gandalf.epitech.eu/mod/assign/index.php?id=[id]";
 
-const semestersDates = [
-  {
-    name: "T5",
-    start: "2023-09-12",
-    end: "2024-02-12",
-  },
-  {
-    name: "T6",
-    start: "2024-02-12",
-    end: "2024-08-12",
-  },
-  {
-    name: "T7",
-    start: "2024-08-12",
-    end: "2025-02-12",
-  },
-  {
-    name: "T8",
-    start: "2025-02-12",
-    end: "2025-08-12",
-  },
-  {
-    name: "T9",
-    start: "2025-08-12",
-    end: "2026-02-12",
-  },
-  {
-    name: "T10",
-    start: "2026-02-12",
-    end: "2026-08-12",
-  },
-];
-
 const cleanFiles = () => {
   if (fs.existsSync(files.otp)) {
     fs.unlinkSync(files.otp);
@@ -291,7 +258,6 @@ cleanFiles();
     created_at: null,
   };
 
-  // For each course we retrieve the assignments
   for (let i = 0; i < coursesCount; i++) {
     const course = courses[i];
     const element = await course.$("a");
@@ -318,19 +284,7 @@ cleanFiles();
       );
 
       if (semesterName === null) {
-        for (const semesterDate of semestersDates) {
-          if (
-            new Date(semesterDate.start) <= new Date() &&
-            new Date(semesterDate.end) >= new Date()
-          ) {
-            semesterName = semesterDate.name;
-            break;
-          }
-        }
-      }
-
-      if (semesterName === null) {
-        semesterName = "-";
+        semesterName = "Global";
       }
 
       const table = await coursePage.$x(
