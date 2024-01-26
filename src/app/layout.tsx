4bd7e29911602592950967518c46bf3c52fafbc1
AlexisHenry <alexis.henry150357@gmail.com>
2024-01-27 00:09:34 +0100

    feat: setup pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,22 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Epitech Grades",
-  description: "Frontend for see your grades at Epitech easily",
+  title: "Epigrades",
+  description: "Faster than Gandalf 🤭",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["epigrades", "grades", "epitech", "strasbourg", "epi", "gandalf"],
+  authors: [
+    { name: "Alexis Henry" },
+    {
+      name: "Alexis Henry",
+      url: "https://www.linkedin.com/in/alexishenry03/",
+    },
+  ],
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
 
 export default function RootLayout({
