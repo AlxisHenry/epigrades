af525ac914a8ab67d96c78a3a5915cabfbfe34c5
Alexis HENRY <a.henry@opticod.fr>
2024-02-02 11:58:32 +0100

    feat: get the grae average

--- src/services/grades.ts
+++ src/services/grades.ts
@@ -1,8 +1,9 @@
 import { type Course } from "./courses";
 import { type Day } from "./days";
+import { Semester } from "./semesters";
 
 export enum Grade {
-  A = "A", 
+  A = "A",
   B = "B",
   C = "C",
   D = "D",
@@ -13,7 +14,7 @@ export function isValidGrade(grade: string): boolean {
   return Object.values(Grade).includes(grade as Grade);
 }
 
-export function getCourseGrade(course: Course|null): string {
+export function getCourseGrade(course: Course | null): string {
   if (course === null) return "-";
 
   let lastDay: Day = course.days[course.days.length - 1];
@@ -25,4 +26,31 @@ export function getCourseGrade(course: Course|null): string {
   }
 
   return "-";
+}
+
+export function getMoreRecurentGrade(semester: Semester): string {
+  let grades: string[] = semester.courses.map((course) => getCourseGrade(course));
+  let gradeCount: { [key: string]: number } = {};
+
+  for (let grade of grades) {
+    if (grade !== "-") {
+      if (gradeCount[grade]) {
+        gradeCount[grade] += 1;
+      } else {
+        gradeCount[grade] = 1;
+      }
+    }
+  }
+
+  let maxGrade: string = "-";
+  let maxCount: number = 0;
+
+  for (let grade in gradeCount) {
+    if (gradeCount[grade] > maxCount) {
+      maxGrade = grade;
+      maxCount = gradeCount[grade];
+    }
+  }
+
+  return maxGrade;
 }
